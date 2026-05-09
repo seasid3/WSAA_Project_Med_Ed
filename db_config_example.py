ae9364f05479c25d1935374c6080e0e32da2b7ee
Orla Woods <orla.woods@gmail.com>
2026-05-09 17:26:48 +0000

    Remove redundant db_config_example.py

--- db_config_example.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    "host":     "YOUR_USERNAME.mysql.pythonanywhere-services.com",
-    "user":     "YOUR_USERNAME",
-    "password": "YOUR_PASSWORD",
-    "database": "YOUR_USERNAME$rcppi_bst"
-}
